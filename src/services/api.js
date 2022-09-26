e9d31b8061990750565fffd0e61693334c80a8ea
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2022-09-25 21:35:38 -0300

    fix service/api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const api = Axios.create({
-  baseURL: 'http://localhost:5001',
+  baseURL: process.env.VITE_BASE_URL,
   headers: {
     contentType: 'Application/json'
   }
